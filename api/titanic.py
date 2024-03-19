c2361bde8988cabc58d1e1d3edcfa666e3174477
SrijDude3416 <srij.0504@gmail.com>
2024-03-19 13:35:15 -0700

    added titanic model and api and everything

--- api/titanic.py
+++ api/titanic.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, jsonify, current_app, Response
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 from auth_middleware import token_required
+import pdb
 
 from model.titanic import Titanic #imports titanic data
 
@@ -13,29 +14,34 @@ titanic_api = Blueprint('titanic_api', __name__,
 api = Api(titanic_api)
 class TitanicAPI:        
     class Predict(Resource): 
-        def predict(self):
+        def post(self):
             body = request.get_json()
+            print(body)
             # Initializing all parameters
-            Pclass = body.get('Pclass')
-            if age is None:
-                return {'message': f'Pclass is missing'}, 400
-            age = body.get('age')
+            pclass = body.get('Pclass')
+            if pclass is None:
+                return {'message': f'pclass is missing'}, 400
+            age = body.get('Age')
             if age is None:
                 return {'message': f'Age is missing'}, 400
-            Sex = body.get('Sex')
-            if Sex is None:
+            sex = body.get('Sex')
+            if sex is None:
                 return {'message': f'Sex is missing'}, 400
-            fare = body.get('fare')
+            fare = body.get('Fare')
             if fare is None:
                 return {'message': f'Fare is missing'}, 400
-            SibSp = body.get('SibSp')
-            if SibSp is None:
-                return {'message': f'SibSp is missing'}, 400
-            Parch = body.get('Parch')
-            if Parch is None:
-                return {'message': f'Parch is missing'}, 400
+            sibsp = body.get('SibSp')
+            if sibsp is None:
+                return {'message': f'sibsp is missing'}, 400
+            parch = body.get('Parch')
             
-            info = [Pclass,age,Sex,SibSp,Parch,fare]
-            result = Titanic.predict(info)
+            if parch is None:
+                return {'message': f'parch is missing'}, 400
+            
+            info = [pclass,age,sex,sibsp,parch,fare]
+
+            result = Titanic.predict(body)
             
             return { 'message': result }
+        
+    api.add_resource(Predict, '/predict')
--- model/titanic.py
+++ model/titanic.py
@@ -14,7 +14,7 @@ class Titanic():
     def initialize():
         data_train = pd.read_csv('./train.csv')
         data_train['Age'].fillna(data_train['Age'].mean(), inplace=True)
-        data_train['Sex'] = data_train['Sex'].map({'male': 0, 'female': 1})
+        data_train['Sex'] = data_train['Sex'].map({'male': 1, 'female': 0})
         X = data_train[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
         y = data_train['Survived']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -25,12 +25,13 @@ class Titanic():
         with open(save_path, 'wb') as model_file:
             pickle.dump(log_reg, model_file)
 
-       
     #userInput=[pclass, sex, age, sibsp,fare]
-    def predict(userInput, y_test):
+    def predict(userInput):
         save_path = "logistic_regression_model.pkl"
+        userInput = pd.DataFrame.from_dict([userInput])
         with open(save_path, 'rb') as model_file:
             loaded_model = pickle.load(model_file)
-        _, alive_proba = loaded_model.predict_proba(userInput)
-        return alive_proba
+        survival = loaded_model.predict_proba(userInput)[0]
+        print(survival)
+        return survival[1]
     
